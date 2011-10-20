04221f134e88083ff959b75c1fab4d45f0eded2d
Alejandro Gomez <alejandroogomez@gmail.com>
2011-10-20 17:33:58 +0200

    keymappings deleted

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -18,8 +18,4 @@ xmonad $ defaultConfig
                     , ppTitle = xmobarColor "green" "" . shorten 50
                     }
     , modMask = mod4Mask -- Rebind Mod to the Windows key
-    } `additionalKeys`
-    [ ((mod4Mask .|. shiftMask, xK_z), spawn "xscreensaver-command -lock")
-    , ((controlMask, xK_Print), spawn "sleep 0.2; scrot -s")
-    , ((0, xK_Print), spawn "scrot")
-    ]                              
+    }
