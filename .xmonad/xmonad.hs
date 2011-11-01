3b82e418b5a6660d8f55ce980ff260d196f3d73c
Alejandro Gomez <alejandroogomez@gmail.com>
2011-11-01 11:03:59 +0100

    xmobar config inside xmonad.hs

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -1,10 +1,10 @@
 -- Haskell libraries
 import Data.Ratio                   ((%))
 import System.IO
-
+-- XMonad
 import XMonad
 -- Hooks
-import XMonad.Hooks.DynamicLog      (PP(..), dynamicLogWithPP, shorten, xmobarPP, xmobarColor)
+import XMonad.Hooks.DynamicLog
 import XMonad.Hooks.ManageDocks
 -- Layout
 import XMonad.Layout                
@@ -12,30 +12,61 @@ import XMonad.Layout.NoBorders      (noBorders, smartBorders)
 import XMonad.Layout.Grid
 import XMonad.Layout.Tabbed
 -- Utils
+import XMonad.Util.Font
 import XMonad.Util.Run              (spawnPipe)
+import XMonad.Util.Loggers
 
- 
+
 main = do 
-        xmproc <- spawnPipe "xmobar"
+        bar <- myBar
         xmonad $ defaultConfig
-            { borderWidth = 2
-            , normalBorderColor = "#CCCCCC"
-            , focusedBorderColor = "#FF0099" 
-            , workspaces = ["1:web", "2:wiki", "3:dev"] ++ map show [4..9 :: Int]
+            { terminal = myTerminal 
+            , borderWidth = myBorderWidth
+            , normalBorderColor = myNormalBorderColor
+            , focusedBorderColor = myFocusedBorderColor
+            , workspaces = myWorkspaces
             , manageHook = myManageHook
             , layoutHook = myLayoutHook
-            , logHook = dynamicLogWithPP xmobarPP
-                { ppOutput = hPutStrLn xmproc
-                , ppTitle = xmobarColor "white" "blue" . shorten 50
-                }
-            -- Win or Cmd key as mod key
-            , modMask = mod4Mask
+            , logHook = myXmobarPP bar
+            , modMask = myModMask
             }
+
+--
+-- xmonad 
+--
+myTerminal = "xterm"
+myWorkspaces = ["1:web", "2:wiki", "3:dev"] ++ map show [4..9 :: Int]
+myModMask = mod4Mask     -- Win or Cmd key 
+
+
+myBorderWidth = 2
+myNormalBorderColor = "#CCCCCC"
+myFocusedBorderColor = "#FF0099"
+
+--
+-- UI
+--
+myDzenPP = dzenPP
+myXmobarPP bar = dynamicLogWithPP xmobarPP
+                    { ppCurrent = xmobarColor "yellow" "" . wrap "|" "|"
+                    , ppHidden = xmobarColor "gray" ""
+                    {-, ppHiddenNoWindows = -}
+                    {-, ppUrgent = -}
+                    , ppSep = " - " 
+                    , ppWsSep = "·" 
+                    , ppTitle = xmobarColor "black" "white" . shorten 50
+                    , ppLayout = xmobarColor "green" "" . wrap "_" "_"
+                    , ppOrder = \(ws:l:t:xs) -> [t,ws,l] ++ xs
+                    , ppExtras = [battery, date "%H:%M · %a %d.%m.%Y"]
+                    , ppOutput = hPutStrLn bar
+                    }
+
+myBar = spawnPipe "xmobar"
 
 -- 
 -- layouts
 --
-myLayoutHook = smartBorders . avoidStruts $ tiled ||| Mirror tiled ||| simpleTabbed ||| Full ||| Grid
+myLayoutHook = smartBorders . avoidStruts $ tiled ||| Mirror tiled ||| Full ||| Grid
             where
                 tiled = Tall masterWindows delta ratio
                 masterWindows = 1
@@ -43,6 +74,6 @@ myLayoutHook = smartBorders . avoidStruts $ tiled ||| Mirror tiled ||| simpleTab
                 ratio = 1/2
 
 --
--- application specific settings
+-- hooks
 --
 myManageHook = manageDocks <+> manageHook defaultConfig 
