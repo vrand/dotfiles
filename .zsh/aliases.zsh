b0b0f2f9195dfa0260546de0e46fd222a233f52f
Alejandro Gomez <alejandroogomez@gmail.com>
2011-11-09 01:03:24 +0100

    aliases for zsh

---
+++ .zsh/aliases.zsh
@@ -0,0 +1,18 @@
+# Directory tree navigation
+alias -g ..='cd ..'
+alias -g ...='cd ../..'
+
+# Files by suffix
+alias -s c=$EDITOR
+alias -s com=$BROWSER
+alias -s h=$EDITOR
+alias -s html=$BROWSER
+alias -s js=$EDITOR
+alias -s org=$BROWSER
+alias -s py=$EDITOR
+
+# Useful
+alias -g LC='| wc -l'
+alias -g DN='/dev/null'
+alias -g L='| less'
+alias -g NUL='> /dev/null 2>&1'
