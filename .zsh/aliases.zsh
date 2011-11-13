759ebd9a4958019a680bf7c45ab86976b36387c9
Alejandro Gomez <alejandroogomez@gmail.com>
2011-11-13 20:08:56 +0100

    Aliases and completion

--- .zsh/aliases.zsh
+++ .zsh/aliases.zsh
@@ -4,10 +4,10 @@ alias -g ...='cd ../..'
 
 # Shortcuts
 alias cl=clear
-alias g=git
+alias -g g=git
 alias ll='ls -lA'
-alias s=sudo
-alias v=vim
+alias -g s=sudo
+alias -g v=vim
 alias yao=yaourt
 
 # Files by suffix
@@ -15,11 +15,13 @@ alias -s c=$EDITOR
 alias -s com=$BROWSER
 alias -s h=$EDITOR
 alias -s html=$BROWSER
+alias -s java=$EDITOR
 alias -s jpg=feh
 alias -s js=$EDITOR
 alias -s org=$BROWSER
 alias -s png=feh
 alias -s py=$EDITOR
+alias -s PKGBUILD=$EDITOR
 
 # Useful
 alias -g G='| grep'
--- .zsh/completion.zsh
+++ .zsh/completion.zsh
@@ -1,2 +1,13 @@
 autoload -U compinit
 compinit
+
+setopt completealiases
+
+# General completion
+zstyle ':completion:*' completer _complete _match _approximate
+zstyle ':completion:*:match:*' original only
+zstyle ':completion:*:approximate:*' max-errors 1 numeric
+
+# Kill completion
+zstyle ':completion:*:*:kill:*' menu yes select
+zstyle ':completion:*:kill:*' force-list always
