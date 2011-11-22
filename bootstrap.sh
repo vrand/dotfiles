5121e6751047a3da0641fb0520134b57b765b2ae
Alejandro Gomez <alejandroogomez@gmail.com>
2011-11-22 15:19:45 +0100

    curl operations in background

--- bootstrap.sh
+++ bootstrap.sh
@@ -5,11 +5,11 @@ rsync --exclude ".git/" --exclude ".DS_Store" --exclude "bootstrap.sh" --exclude
 
 # grab last version of pathogen VIM plugin
 mkdir -p ~/.vim/autoload ~/.vim/bundle
-curl -so ~/.vim/autoload/pathogen.vim https://raw.github.com/tpope/vim-pathogen/HEAD/autoload/pathogen.vim
+curl -so ~/.vim/autoload/pathogen.vim https://raw.github.com/tpope/vim-pathogen/HEAD/autoload/pathogen.vim &
 
 # grab last versions of some pentadactyl plugins
 mkdir -p ~/.pentadactyl/plugins
-curl -so ~/.pentadactyl/plugins/smooth-scroll.js http://dactyl.sourceforge.net/plugins/smooth-scroll.js
-curl -so ~/.pentadactyl/plugins/flashblock.js http://dactyl.sourceforge.net/plugins/flashblock.js
+curl -so ~/.pentadactyl/plugins/smooth-scroll.js http://dactyl.sourceforge.net/plugins/smooth-scroll.js &
+curl -so ~/.pentadactyl/plugins/flashblock.js http://dactyl.sourceforge.net/plugins/flashblock.js &
 
 source ~/.zshrc
