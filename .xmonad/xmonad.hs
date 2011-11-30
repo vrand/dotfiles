ab5255d11910662a544d95eae1f79dfc332f2a4d
Alejandro Gómez <alejandroogomez@gmail.com>
2011-11-30 20:47:09 +0100

    aterm instead of xterm, sexy dmenu, background

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -1,6 +1,7 @@
 -- Haskell libraries
 import Data.Ratio
 import System.IO
+import qualified Data.Map as M
 -- XMonad
 import XMonad
 -- Hooks
@@ -9,8 +10,10 @@ import XMonad.Hooks.ManageDocks
 -- Layout
 import XMonad.Layout                
 import XMonad.Layout.NoBorders     
+import XMonad.Layout.Spacing
 import XMonad.Layout.Tabbed
 -- Utils
+import XMonad.Util.EZConfig
 import XMonad.Util.Run             
 import XMonad.Util.Loggers
 
@@ -24,15 +27,16 @@ main = do
             , focusedBorderColor = myFocusedBorderColor
             , workspaces         = myWorkspaces
             , manageHook         = myManageHook
+--            , keys               = myKeys
             , layoutHook         = myLayoutHook
             , logHook            = dynamicLogWithPP $ myXmobar bar
             , modMask            = myModMask
-            }
-
+            } `additionalKeys`
+            [((mod4Mask, xK_p), spawn myDmenuCmd)] 
 --
 -- xmonad 
 --
-myTerminal   = "xterm"
+myTerminal   = "aterm"
 myWorkspaces = ["dev", "test", "web", "mail", "media", "system", "extra"]
 myModMask    = mod4Mask     -- Win or Cmd key
 
@@ -65,14 +69,31 @@ myXmobar bar = defaultPP
 -- 
 -- layouts
 --
-myLayoutHook = smartBorders . avoidStruts $ tiled        |||
-                                            Mirror tiled |||
-                                            Full         
+myLayoutHook = spacing 3 $ smartBorders . avoidStruts $ tiled        |||
+                                                        Mirror tiled |||
+                                                        Full         
                 where
                     tiled         = Tall masterWindows delta ratio
                     masterWindows = 1
                     delta         = 1/10
                     ratio         = 3/4
+
+--
+-- keys
+--
+myDmenuCmd = "exe=`dmenu_run -i -b -nb cyan -nf black -sb yellow -sf black -l 5` && eval \"exec $exe\""
+
+{-keysToDel x = [(mod4Mask, xK_p)]-}
+{---               (mod4Mask, xK_h),-}
+{---               (mod4Mask, xK_l)]-}
+
+{-keysToAdd x = [((mod4Mask, xK_p), spawn myDmenuCmd)]-}
+
+{--- include custom keys-}
+{-newKeys x = M.union (keys defaultConfig x) (M.fromList (keysToAdd x))-}
+
+{--- delete unused keys-}
+{-myKeys x = foldr M.delete (newKeys x) (keysToDel x) -}
 
 --
 -- hooks
