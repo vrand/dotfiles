74ce99abb7f905ac2dbafb3f97446023df048e11
Alejandro Gómez <alejandroogomez@gmail.com>
2011-12-08 01:39:57 +0100

    environment variables

--- .zsh/environment.zsh
+++ .zsh/environment.zsh
@@ -3,4 +3,15 @@ export WORKON_HOME=$HOME/.virtualenvs
 export PROJECT_HOME=$HOME/src
 source /usr/bin/virtualenvwrapper.sh
 
+export EDITOR=vim
+
+# browser
+if [[ -n $DISPLAY ]] then
+    export BROWSER=elinks
+else
+    export BROWSER=firefox
+fi
+export WWW_HOME=http://dialelo.com
+
+# path
 export PATH=$HOME/.cabal/bin:$PATH
