f3c39f135093b0665440ec30d0c0e7508ca9699f
Alejandro Gómez <alejandroogomez@gmail.com>
2011-12-15 14:13:55 +0100

    kustom keybindings

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -4,15 +4,24 @@ import System.IO
 import qualified Data.Map as M
 -- XMonad
 import XMonad
+-- Actions
+import XMonad.Actions.DynamicWorkspaces
+import XMonad.Actions.CycleWS
 -- Hooks
 import XMonad.Hooks.DynamicLog
 import XMonad.Hooks.ManageDocks
+-- Prompt
+import XMonad.Prompt
+import XMonad.Prompt.Man
+import XMonad.Prompt.Shell
+import XMonad.Prompt.XMonad
 -- Layout
 import XMonad.Layout                
 import XMonad.Layout.NoBorders     
 import XMonad.Layout.Spacing
 import XMonad.Layout.Tabbed
 -- Utils
+import XMonad.Util.CustomKeys
 import XMonad.Util.EZConfig
 import XMonad.Util.Run             
 import XMonad.Util.Loggers
@@ -27,18 +36,18 @@ main = do
             , focusedBorderColor = myFocusedBorderColor
             , workspaces         = myWorkspaces
             , manageHook         = myManageHook
+            , keys               = myKeys
             , layoutHook         = myLayoutHook
             , logHook            = dynamicLogWithPP $ myXmobar bar
             , modMask            = myModMask
-            } `removeKeys`         keysToDel
-              `additionalKeys`     keysToAdd 
+            } 
               
 
 --
 -- xmonad 
 --
 myTerminal   = "urxvt"
-myWorkspaces = ["dev", "test", "web", "mail", "media", "system", "extra"]
+myWorkspaces = ["dev", "web"]
 myModMask    = mod4Mask     -- Win or Cmd key
 
 
@@ -48,21 +57,22 @@ myModMask    = mod4Mask     -- Win or Cmd key
 myBorderWidth        = 2
 myNormalBorderColor  = "#CCCCCC"
 myFocusedBorderColor = "#FF0099"
+myFont               = "-xos4-terminus-bold-r-normal--22-220-72-72-c-110-iso8859-1"
 
 --
 -- status bar
 --
 myBar = spawnPipe "xmobar"
 
--- ther is more customization to xmobar in the .xmobarrc file
+-- there is more customization to xmobar in the .xmobarrc file
 myXmobar bar = defaultPP
-                { ppCurrent = xmobarColor "black" "gray"
+                { ppCurrent = xmobarColor "cyan" ""
                 , ppHidden  = xmobarColor "white" ""
                 , ppSep     = " - "
                 , ppWsSep   = "·"
-                , ppTitle   = xmobarColor "black" "#ee9a00" . shorten 60
-                , ppLayout  = xmobarColor "black" "cyan"
-                , ppOrder   = \(ws:l:t:xs) -> [ws,l,t] ++ xs
+                , ppTitle   = xmobarColor "#FF6666" "" . shorten 60
+              --, ppLayout  = xmobarColor "black" "cyan"
+                , ppOrder   = \(ws:_:t:xs) -> [ws,t] ++ xs
                 , ppOutput  = hPutStrLn bar
                 }
 
@@ -70,7 +80,7 @@ myXmobar bar = defaultPP
 -- 
 -- layouts
 --
-myLayoutHook = spacing 3 $ smartBorders . avoidStruts $ tiled        |||
+myLayoutHook = spacing 3 $ avoidStruts . smartBorders $ tiled        |||
                                                         Mirror tiled |||
                                                         Full         
                 where
@@ -80,21 +90,59 @@ myLayoutHook = spacing 3 $ smartBorders . avoidStruts $ tiled        |||
                     ratio         = 3/4
 
 --
+-- prompt
+--
+myPromptBgColor       = "#CCCCCC"
+myPromptFgColor       = "#000000"
+myPromptBgHLightColor = "#000000"
+myPromptFgHLightColor = "#CC3333"
+myPromptBorderWidth   = 0
+myPromptHistorySize   = 10000
+myPromptHeight        = 25
+
+myPrompt :: XPConfig
+myPrompt = defaultXPConfig
+            { position          = Top
+            , font              = myFont
+            , bgColor           = myPromptBgColor
+            , fgColor           = myPromptFgColor
+            , fgHLight          = myPromptFgHLightColor
+            , bgHLight          = myPromptBgHLightColor
+            , promptBorderWidth = myPromptBorderWidth
+            , height            = myPromptHeight
+            }
+
+--
 -- keys
 --
-myDmenuCmd = "exe=`dmenu_run -i -b -nb cyan -nf black -sb black -sf yellow -l 5` && eval \"exec $exe\""
+keysToDel :: XConfig l -> [(KeyMask, KeySym)] 
+keysToDel XConfig {modMask = modm} = 
+            [ ((mod4Mask,                xK_p))
+            ]
 
-keysToAdd = [((mod4Mask, xK_p), spawn myDmenuCmd), 
-             ((mod4Mask, xK_l), spawn "luakit"),   
-             ((mod4Mask, xK_f), spawn "firefox"),   
-             ((mod4Mask, xK_i), sendMessage Expand),
-             ((mod4Mask, xK_o), sendMessage Shrink)
+keysToAdd :: XConfig l -> [((KeyMask, KeySym), X ())]
+keysToAdd conf@(XConfig {modMask = modm}) = 
+            [ ((mod4Mask,               xK_f)        , spawn "firefox")                          
+             -- toggle between last two workspaces
+            , ((mod4Mask,               xK_a)        , toggleWS)                                 
+             -- prompt for a workspace's name and move to it
+            , ((mod4Mask,               xK_s)        , selectWorkspace myPrompt)                 
+             -- rename the current workspace
+            , ((mod4Mask,               xK_comma)    , renameWorkspace myPrompt)                 
+             -- add a workspace
+            , ((mod4Mask,               xK_n)        , withWorkspace myPrompt addHiddenWorkspace)
+             -- remove current workspace (potentially dangerous)
+            , ((mod4Mask .|. shiftMask, xK_BackSpace), removeWorkspace)   
+             -- quickly search for man pages 
+            , ((mod4Mask,               xK_m)        , manPrompt myPrompt)
+             -- prompt for XMonad actions 
+            , ((mod4Mask,               xK_x)        , xmonadPrompt myPrompt)
+             -- shell
+            , ((mod4Mask,               xK_p)        , shellPrompt myPrompt)
             ] 
 
-keysToDel = [(mod4Mask, xK_p),
-             (mod4Mask, xK_h),
-             (mod4Mask, xK_l)
-            ]
+myKeys    = customKeys keysToDel keysToAdd
+
 
 --
 -- hooks
