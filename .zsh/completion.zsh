f036e485ddc34306c7ac82396611bb3b4d3a4a5c
Alejandro Gómez <alejandroogomez@gmail.com>
2011-12-22 20:45:21 +0100

    pip autocompletion and configuration

--- .zsh/completion.zsh
+++ .zsh/completion.zsh
@@ -11,3 +11,16 @@ zstyle ':completion:*:approximate:*' max-errors 1 numeric
 # Kill completion
 zstyle ':completion:*:*:kill:*' menu yes select
 zstyle ':completion:*:kill:*' force-list always
+
+# pip zsh completion start
+function _pip_completion {
+  local words cword
+  read -Ac words
+  read -cn cword
+  reply=( $( COMP_WORDS="$words[*]" \
+             COMP_CWORD=$(( cword-1 )) \
+             PIP_AUTO_COMPLETE=1 $words[1] ) )
+}
+compctl -K _pip_completion pip
+# pip zsh completion end
+
