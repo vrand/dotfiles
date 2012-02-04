f3c31c0c819e74db2d3ee8d3d8737111abeaf2ac
Alejandro Gómez <alejandroogomez@gmail.com>
2012-02-04 15:07:57 +0100

    history

--- .zsh/environment.zsh
+++ .zsh/environment.zsh
@@ -1,8 +1,21 @@
+# misc
+setopt autocd
+
+# history
+export HISTSIZE=10000
+export HISTFILE="$HOME/.history"
+
+export SAVEHIST=$HISTSIZE
+
+setopt hist_ignore_all_dups
+setopt hist_ignore_space
+
 # virtualenvwrapper configuration
 export WORKON_HOME=$HOME/.virtualenvs
 export PROJECT_HOME=$HOME/src
 source /usr/bin/virtualenvwrapper.sh
 
+# editor
 export EDITOR=vim
 
 # browser
