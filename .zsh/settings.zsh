e6936cc23b86486d615d1001cbbf5bc750aa6d9b
Alejandro Gómez <alejandroogomez@gmail.com>
2012-05-26 09:20:48 +0200

    .zshenv back to .zsh/; .zsh/settings.zsh

---
+++ .zsh/settings.zsh
@@ -0,0 +1,8 @@
+# cd into a directory typing its name
+setopt autocd
+
+# ignore duplicates in history
+setopt hist_ignore_all_dups
+# ignore leading and trailing whitespace in history
+setopt hist_ignore_space
+
