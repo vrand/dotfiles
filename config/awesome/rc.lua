d77317d87150cfaf723c4b07931b25b2895376ba
Alejandro Gómez <alejandro@dialelo.com>
2013-01-02 23:38:01 +0100

    Don't require naughty

--- config/awesome/rc.lua
+++ config/awesome/rc.lua
@@ -4,8 +4,6 @@ require("awful.autofocus")
 require("awful.rules")
 -- Theme handling library
 require("beautiful")
--- Notification library
-require("naughty")
 -- Dynamic tagging
 require("eminent")
 -- Scratchpad
