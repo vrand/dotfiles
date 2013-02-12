6f6169973110198cfb33904bfededc8a18454c24
Alejandro Gómez <alejandro@dialelo.com>
2013-02-12 10:05:41 +0100

    Launch mutt with a key binding

--- config/awesome/rc.lua
+++ config/awesome/rc.lua
@@ -212,6 +212,7 @@ globalkeys = awful.util.table.join(
     -- Standard program
     awful.key({ modkey,           }, "Return", function () awful.util.spawn(terminal) end),
     awful.key({ modkey,           }, "b", function () awful.util.spawn(browser) end),
+    awful.key({ modkey, "Shift"   }, "m", function () awful.util.spawn(terminal .. " -e mutt -R") end),
     awful.key({ modkey, "Control" }, "r", awesome.restart),
     awful.key({ modkey, "Shift"   }, "q", awesome.quit),
 
